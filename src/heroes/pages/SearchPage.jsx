84937bde68827bccb0723bb76f03e102a7bae311
Aldhair Vera <aldhair@subterra.ai>
2022-09-13 22:54:40 -0500

    Search Page

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -20,7 +20,7 @@ export const SearchPage = () => {
   const handleSearchSubmit = (event) => {
     event.preventDefault();
 
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     console.log({ searchText });
     navigate(`?q=${searchText.toLowerCase().trim()}`);
@@ -53,11 +53,17 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          {heroes.length <= 0 && (
-            <div className="alert alert-danger">
-              No hero with <b> {q} </b>
+
+          {q === "" ? (
+            <div className="alert alert-primary animate__animated animate__fadeIn">
+              Search a hero
             </div>
+          ) : (
+            heroes.length <= 0 && (
+              <div className="alert alert-danger animate__animated animate__fadeIn">
+                No hero with <b> {q} </b>
+              </div>
+            )
           )}
 
           {heroes.map((hero) => (
