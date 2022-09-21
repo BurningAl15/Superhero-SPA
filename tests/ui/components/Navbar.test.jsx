216f4b657f20418577f23f5fe034458f9e666d22
Aldhair Vera <aldhair@subterra.ai>
2022-09-20 23:57:51 -0500

    Testing Navbar

---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../src/auth";
+import { AppRouter } from "../../../src/router/AppRouter";
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+}));
+
+describe("Pruebas en <Navbar />", () => {
+  const contextValue = {
+    logged: true,
+    user: {
+      name: "Aldhair Vera",
+      id: "ABC123",
+    },
+    logout: jest.fn(),
+  };
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test("debe de mostrar el nombre del usuario", () => {
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/marvel"]}>
+          <AppRouter />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText("Aldhair Vera")).toBeTruthy();
+  });
+
+  test("debe de llamar el logout y navigate cuando hace click en el botón", () => {
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/marvel"]}>
+          <AppRouter />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    const logoutBtn = screen.getByRole("button");
+    fireEvent.click(logoutBtn);
+
+    expect(contextValue.logout).toHaveBeenCalled();
+    expect(mockedUseNavigate).toHaveBeenCalledWith("/login", { replace: true });
+  });
+});
