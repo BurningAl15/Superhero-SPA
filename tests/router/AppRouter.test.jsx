354695da8e2355d9044e4d3413e2840a49b3a111
Aldhair Vera <aldhair@subterra.ai>
2022-09-20 23:24:31 -0500

    Testing AppRouter

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AuthContext } from "../../src/auth";
+import { AppRouter } from "../../src/router/AppRouter";
+
+describe("Pruebas en <AppRouter />", () => {
+  test("debe de mostrar el login si no está autenticado", () => {
+    const contextValue = {
+      logged: false,
+    };
+
+    render(
+      <MemoryRouter initialEntries={["/marvel"]}>
+        <AuthContext.Provider value={contextValue}>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByText("Login").length).toBe(2);
+  });
+
+  test("debe de mostrar el componente marvel si está autenticado", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: "Aldhair Vera",
+        id: "ABC123",
+      },
+    };
+
+    render(
+      <MemoryRouter initialEntries={["/marvel"]}>
+        <AuthContext.Provider value={contextValue}>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Logout")).toBeTruthy();
+    expect(screen.getAllByText("Marvel").length).toBeGreaterThanOrEqual(1);
+
+    // screen.debug();
+  });
+});
